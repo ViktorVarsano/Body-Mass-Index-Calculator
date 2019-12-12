a49f7b899cf4dbbd38e916ba28697d7ef6420d56
Viktor Varsano <Viktor_varsano@hotmail.com>
2019-12-12 19:19:24 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Starting point for all our apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Let\'s build a BMI Calculator'),
+      ),
+    ),
+  );
+}
