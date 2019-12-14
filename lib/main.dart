a9a45ab905f4a149a398d1d20fa0b115c07b0a16
Viktor Varsano <Viktor_varsano@hotmail.com>
2019-12-14 12:49:35 +0200

    Added floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 //Starting point for all our apps
-void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Let\'s build a BMI Calculator'),
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
       ),
-    ),
-  );
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
